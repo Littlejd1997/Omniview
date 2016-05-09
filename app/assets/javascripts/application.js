16ed6ad519e4961c2a08f64bb54c62f7bbec781c
Jonathan Schober <jonathan@Home-iMac.local>
2016-05-08 22:00:21 -0500

    undo move

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,3 @@
 //= require bootstrap
 
 
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=289099567946243";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
